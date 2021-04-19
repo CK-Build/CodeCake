493dcee249d90f0350906a7dd1ef96798880ab3e
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-04-19 10:17:45 +0200

    Revert "Upgraded Cake version (1.1.0)."
    This reverts commit 6825d230199135fc64d93206c20e4911a0050721.

--- Code.Cake/CakeArguments.cs
+++ Code.Cake/CakeArguments.cs
@@ -1,32 +1,81 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Cake.Core;
 
 namespace CodeCake
 {
     internal sealed class CakeArguments : ICakeArguments
     {
-        private readonly Dictionary<string, string[]> _arguments;
+        private readonly Dictionary<string, string> _arguments;
 
+        /// <summary>
+        /// Gets the arguments.
+        /// </summary>
+        /// <value>The arguments.</value>
+        public IReadOnlyDictionary<string, string> Arguments
+        {
+            get { return _arguments; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CakeArguments"/> class.
+        /// </summary>
+        public CakeArguments()
+        {
+            _arguments = new Dictionary<string, string>( StringComparer.OrdinalIgnoreCase );
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CakeArguments"/> class based on the given dictionary of arguments.
+        /// </summary>
+        /// <param name="arguments">The arguments.</param>
         public CakeArguments( IDictionary<string, string> arguments )
         {
-            _arguments = new Dictionary<string, string[]>( StringComparer.OrdinalIgnoreCase );
-            foreach( var kv in arguments )
+            if( arguments == null )
             {
-                _arguments.Add( kv.Key, new[] { kv.Key } );
+                throw new ArgumentNullException( "arguments" );
+            }
+            _arguments = new Dictionary<string, string>( arguments, StringComparer.OrdinalIgnoreCase );
+        }
+
+        /// <summary>
+        /// Initializes the argument list.
+        /// </summary>
+        /// <param name="arguments">The arguments.</param>
+        public void SetArguments( IDictionary<string, string> arguments )
+        {
+            if( arguments == null )
+            {
+                throw new ArgumentNullException( "arguments" );
+            }
+            _arguments.Clear();
+            foreach( var argument in arguments )
+            {
+                _arguments.Add( argument.Key, argument.Value );
             }
         }
 
+        /// <summary>
+        /// Determines whether or not the specified argument exist.
+        /// </summary>
+        /// <param name="name">The argument name.</param>
+        /// <returns>
+        ///   <c>true</c> if the argument exist; otherwise <c>false</c>.
+        /// </returns>
         public bool HasArgument( string name )
         {
             return _arguments.ContainsKey( name );
         }
 
-        public ICollection<string> GetArguments( string name )
+        /// <summary>
+        /// Gets an argument.
+        /// </summary>
+        /// <param name="name">The argument name.</param>
+        /// <returns>The argument value.</returns>
+        public string GetArgument( string name )
         {
-            return _arguments.TryGetValue( name, out var a ) ? a : Array.Empty<string>();
+            return _arguments.ContainsKey( name )
+                ? _arguments[name] : null;
         }
     }
-
 }
