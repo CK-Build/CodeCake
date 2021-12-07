683bb6ca419df34b5bb0dd6da5c166c5c9e14ff0
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-12-07 13:56:24 +0100

    Upgraded Cake.Common to 2.0.0.  (...) Applied Settings.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -1,26 +1,7 @@
-using Cake.Common;
-using Cake.Common.Solution;
+
 using Cake.Common.IO;
-using Cake.Common.Tools.MSBuild;
-using Cake.Common.Tools.NuGet;
 using Cake.Core;
-using Cake.Common.Diagnostics;
-using Code.Cake;
-using Cake.Common.Tools.NuGet.Pack;
-using System.Linq;
 using Cake.Core.Diagnostics;
-using Cake.Common.Tools.NuGet.Restore;
-using System;
-using Cake.Common.Tools.NuGet.Push;
-using SimpleGitVersion;
-using Cake.Common.Text;
-using System.Diagnostics;
-using System.Collections.Generic;
-using Cake.Core.IO;
-using Cake.Common.Build.AppVeyor;
-using Cake.Common.Build;
-using Cake.Common.Tools.DotNetCore;
-using Cake.Common.Tools.DotNetCore.Build;
 
 namespace CodeCake
 {
